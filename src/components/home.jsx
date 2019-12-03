e6da5c4697ad09b2231f36678622b8b78adf8715
ali muhammad <ali.muhammad@paid.com>
2019-12-03 15:57:17 -0500

    homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardHeader,
+  CardFooter
+} from "reactstrap";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Row>
+        <Col
+          md={{ size: 6, offset: 3 }}
+          lg={{ size: 6, offset: 3 }}
+          xs="12"
+          className="justify-content-center"
+          style={{ marginTop: 50 }}
+        >
+          <Card>
+            <CardHeader style={{ backgroundColor: "green" }} tag="h3">
+              Home page
+            </CardHeader>
+            <CardBody>
+              <CardTitle tag="h5">Welcome on Home Page</CardTitle>
+              <CardSubtitle>
+                Please join us or login if you have already an account{" "}
+              </CardSubtitle>
+              <CardText>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </CardText>
+            </CardBody>
+            <CardFooter style={{ textAlign: "right" }}>FullStack</CardFooter>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default HomePage;
